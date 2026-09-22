d3f5ed9b48ec3a489b231970ab52193aa161d442
风月院紫苑 <2565201130@qq.com>
2026-09-22 10:24:53 +0800

    Move building schedule ownership into building domain

---
+++ MyriadCreation/Assets/Scripts/Simulation/Society/Building/BuildingSchedule.cs
@@ -0,0 +1,18 @@
+using System;
+using CivilizationEvolution.Core.Events;
+using CivilizationEvolution.Core.Simulation;
+
+namespace CivilizationEvolution.Simulation.Society.Building
+{
+    public static class BuildingSchedule
+    {
+        public const string ScheduleId = "building.daily";
+
+        public static void Register(ISimulationScheduler scheduler, BuildingSystem buildings)
+        {
+            if (scheduler == null) throw new ArgumentNullException(nameof(scheduler));
+            if (buildings == null) throw new ArgumentNullException(nameof(buildings));
+            scheduler.Register(ScheduleId, SimulationCadence.Daily, 13, _ => buildings.DailyTick());
+        }
+    }
+}
