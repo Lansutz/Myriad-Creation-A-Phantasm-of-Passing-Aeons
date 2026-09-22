9c01914c7654bae11e4f90e12841ed56b928d229
风月院紫苑 <2565201130@qq.com>
2026-09-22 16:18:47 +0800

    refactor: schedule settlement through domain runtime

--- MyriadCreation/Assets/Scripts/Simulation/WorldState/GameWorld.cs
+++ MyriadCreation/Assets/Scripts/Simulation/WorldState/GameWorld.cs
@@ -208,13 +208,7 @@ namespace CivilizationEvolution.Simulation.WorldState
             CivilizationEvolution.Simulation.Politics.PoliticsSchedule.Register(_simulationScheduler, _politicsSimulationSystem);
             CivilizationEvolution.Simulation.Settlement.SettlementSchedule.Register(
                 _simulationScheduler,
-                () => CivilizationEvolution.Simulation.Settlement.SettlementControlSystem.DailyTick(
-                    burgs, tiles, mapWidth, mapHeight, armies),
-                () => _mapActorManager?.Tick(1f),
-                () => _campManager?.Tick(1f),
-                () => CivilizationEvolution.Simulation.Settlement.SettlementEvolutionSystem.DailyTick(this),
-                () => CivilizationEvolution.Simulation.Warfare.SettlementDestructionSystem.DailyTickRecovery(this),
-                () => CivilizationEvolution.Simulation.Settlement.LandAbandonmentSystem.DailyCheckBanditSpawn(this));
+                _settlementSimulationSystem);
             CivilizationEvolution.Simulation.Culture.CultureSchedule.Register(_simulationScheduler, MonthlyCultureStageEvolution);
             CivilizationEvolution.Simulation.Diplomacy.DiplomacySchedule.Register(_simulationScheduler, _diplomacyManager, () => currentDay);
             CivilizationEvolution.Simulation.Warfare.WarfareReligionSchedule.Register(
