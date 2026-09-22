fe25638e302a93ea4c8d78870b8c0c0e4489bc11
风月院紫苑 <2565201130@qq.com>
2026-09-22 10:27:28 +0800

    Move domain schedule ownership out of GameWorld

---
+++ MyriadCreation/Assets/Scripts/Simulation/Disaster/DisasterSchedule.cs
@@ -0,0 +1,24 @@
+using System;
+using CivilizationEvolution.Core.Simulation;
+
+namespace CivilizationEvolution.Simulation.Disaster
+{
+    public static class DisasterSchedule
+    {
+        public const string ScheduleId = "disaster.daily";
+
+        public static void Register(ISimulationScheduler scheduler, DisasterSystem disasters, DiseaseSystem diseases, Func<int> day, Func<int> year)
+        {
+            if (scheduler == null) throw new ArgumentNullException(nameof(scheduler));
+            if (disasters == null) throw new ArgumentNullException(nameof(disasters));
+            if (diseases == null) throw new ArgumentNullException(nameof(diseases));
+            if (day == null) throw new ArgumentNullException(nameof(day));
+            if (year == null) throw new ArgumentNullException(nameof(year));
+            scheduler.Register(ScheduleId, SimulationCadence.Daily, 11, _ =>
+            {
+                disasters.DailyTick(day(), year());
+                diseases.DailyTick(day(), year());
+            });
+        }
+    }
+}
