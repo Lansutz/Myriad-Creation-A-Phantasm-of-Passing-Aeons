c127ad15e5911d331afaee285c216e5a88be3b3a
风月院紫苑 <2565201130@qq.com>
2026-09-22 16:19:03 +0800

    refactor: compose settlement runtime during initialization

--- MyriadCreation/Assets/Scripts/Simulation/WorldState/GameWorld.Initialization.cs
+++ MyriadCreation/Assets/Scripts/Simulation/WorldState/GameWorld.Initialization.cs
@@ -162,6 +162,18 @@ namespace CivilizationEvolution.Simulation.WorldState
                 _characterManager,
                 societyCache);
 
+            _settlementSimulationSystem = new SettlementSimulationSystem(
+                burgs,
+                tiles,
+                mapWidth,
+                mapHeight,
+                armies,
+                () => _mapActorManager?.Tick(1f),
+                () => _campManager?.Tick(1f),
+                () => CivilizationEvolution.Simulation.Settlement.SettlementEvolutionSystem.DailyTick(this),
+                () => CivilizationEvolution.Simulation.Warfare.SettlementDestructionSystem.DailyTickRecovery(this),
+                () => CivilizationEvolution.Simulation.Settlement.LandAbandonmentSystem.DailyCheckBanditSpawn(this));
+
             RegisterSimulationSchedules();
         }
 
